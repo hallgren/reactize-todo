ff87237322b42dd3a47c9aa62eab499eaacf5e57
Morgan Hallgren <morgan.hallgren@upptec.se>
2014-11-29 21:01:25 +0100

    now handle <input> tags as expected

--- public/js/reactize.js
+++ public/js/reactize.js
@@ -1,11 +1,11 @@
 /**
- * Reactize v0.4.1
+ * Reactize v0.4.2
  */
 (function(exports) {
   var Reactize = {};
 
   var CLASS_NAME_REGEX = /\sclass=/g;
-  var INPUT_TAG_REGEX = /\s(<input[^>]*)(>)/g;
+  var INPUT_TAG_REGEX = /\s<input([^>]*)(>)/g;
 
   Reactize.reactize = function(element) {
     var code = JSXTransformer.transform(
@@ -33,10 +33,25 @@
   // Converts an HTML string into a JSX-compliant string.
   Reactize.htmlToJsx = function(html) {
     html = html.replace(CLASS_NAME_REGEX, " className=");
-    return html.replace(INPUT_TAG_REGEX, "$1 /$2")
+    return html = html.replace(INPUT_TAG_REGEX, "<ReactInput $1 /$2")
   };
 
-  Reactize.version = "0.4.1";
+  var ReactInput = React.createClass({
+    getInitialState: function() {
+      this.props.onChange = this.handleChange
+      return this.props;
+    },
+
+    handleChange: function(event) {
+      this.setState({value: event.currentTarget.value});
+    },
+
+    render: function() {
+      return React.createElement("input", this.state);
+    }
+  });
+
+  Reactize.version = "0.4.2";
 
   exports.Reactize = Reactize;
 })(window);
